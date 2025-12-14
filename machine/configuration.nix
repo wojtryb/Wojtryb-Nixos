7bc7b539bb55c78d06b578f561dde0a3b60ff64c
wojtryb <wojtryb@gmail.com>
2025-12-14 18:05:23 +0100

    Use open nvidia driver

--- machine/configuration.nix
+++ machine/configuration.nix
@@ -166,47 +166,17 @@
     pkgs.zip
   ];
 
-  # xdg.portal.lxqt.enable = true;
-  # xdg.portal.lxqt.styles = [
-  #   pkgs.sweet
-  #   pkgs.sweet-nova
-  # ];
-
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
+  # Enable nvidia driver
+  hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
     powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    open = true;
+    nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
-# targets.genericLinux.enable = true;
 
   programs.steam = {
     enable = true;

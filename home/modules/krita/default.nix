2c63b282c0f1bd4b858c72bb5bbee79a32e5f87b
wojtryb <wojtryb@gmail.com>
2025-12-16 19:58:52 +0100

    Make krita configuration files editable from within the program

--- home/modules/krita/default.nix
+++ home/modules/krita/default.nix
@@ -1,20 +1,24 @@
 { config, pkgs, ... }:
 
+# TODO: Find a way to get current path from nix
+# Path to this position. Relative links cannot be used to reference current file
+let current_path = "${config.home.homeDirectory}/Code/Wojtryb-Nixos/home/modules/krita"; in
 {
   # Copy custom .desktop file with QT_FACTOR_SCALE set to 1
   # Local desktop file overrides global one installed with pkgs.krita
   home.file.".local/share/applications/org.kde.krita.desktop".source = ./org.kde.krita.desktop;
 
-  # Copy krita configuration files
-  home.file.".config/kritarc".source = ./kritarc;
-  home.file.".config/kritadisplayrc".source = ./kritadisplayrc;
-  home.file.".config/kritashortcutsrc".source = ./kritashortcutsrc;
+  # TODO: find where is the topbar config file and include it here 
+  # Configuration files are symlinked to this repository, making them editable from krita
+  home.file.".config/kritarc".source = config.lib.file.mkOutOfStoreSymlink "${current_path}/kritarc";
+  home.file.".config/kritadisplayrc".source = config.lib.file.mkOutOfStoreSymlink "${current_path}/kritadisplayrc";
+  home.file.".config/kritashortcutsrc".source = config.lib.file.mkOutOfStoreSymlink "${current_path}/kritashortcutsrc";
+  home.file.".local/share/krita/input/kritadefault.profile".source = config.lib.file.mkOutOfStoreSymlink "${current_path}/resources/input/kritadefault.profile";
 
   # Copy krita resources
   # Python plugins are only symlinks to repositories that need to be cloned manually
-  home.file.".local/share/krita/color-schemes".source = ./resources/color-schemes;
-  home.file.".local/share/krita/pykrita".source = ./resources/pykrita;
-  home.file.".local/share/krita/input/kritadefault.profile".source = ./resources/input/kritadefault.profile;
-  home.file.".local/share/krita/Krita_5_Starter_pack.bundle".source = ./resources/Krita_5_Starter_pack.bundle;
-  home.file.".local/share/krita/wojtryb-v6-tilt-rightHand.bundle".source = ./resources/wojtryb-v6-tilt-rightHand.bundle;
+  home.file.".local/share/krita/color-schemes".source = "${current_path}/resources/color-schemes";
+  home.file.".local/share/krita/pykrita".source = "${current_path}/resources/pykrita";
+  home.file.".local/share/krita/Krita_5_Starter_pack.bundle".source = "${current_path}/resources/Krita_5_Starter_pack.bundle";
+  home.file.".local/share/krita/wojtryb-v6-tilt-rightHand.bundle".source = "${current_path}/resources/wojtryb-v6-tilt-rightHand.bundle";
 }

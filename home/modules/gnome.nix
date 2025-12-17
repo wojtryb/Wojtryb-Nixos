ceb8d86b5a788d699eeab9917ef699deb36d9771
wojtryb <wojtryb@gmail.com>
2025-12-17 12:36:03 +0100

    Add some of the gnome dconf configurations

--- home/modules/gnome.nix
+++ home/modules/gnome.nix
@@ -9,7 +9,6 @@
   home.sessionVariables = {
     QT_STYLE_OVERRIDE = "adwaita-dark";
     QT_SCALE_FACTOR = 1.5;
-    # QT_QPA_PLATFORMTHEME = "qt6ct";
   };
 
   gtk = {
@@ -18,41 +17,67 @@
       name = "candy-icons";
       package = pkgs.candy-icons;
     };
-    # theme = {
-    #   name = "adwaita-dark";
-    #   package = pkgs.adwaita-qt6;
-    # };
-    # theme = {
-    #   name = "Sweet";
-    #   package = pkgs.sweet;
-    # };
     cursorTheme = {
       name = "Numix-Cursor";
       package = pkgs.numix-cursor-theme;
       size = 48;
     };
-    gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
-    gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
-    };
   };
-  # home.sessionVariables.GTK_THEME = "adwaita-dark";
-  # home.sessionVariables.QT_SCALE_FACTOR = 1.5;
   
   dconf.enable = true;
   dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      accent-color = "orange";
-    };
-    "org/gnome/desktop/input-sources" = {
-      xkb-options = [ "ctrl:nocaps" ];
-    };
-  };
+    # "org/gnome/desktop/input-sources" = { xkb-options = [ "ctrl:nocaps" ]; };
 
-  }
+    # Power settings
+    "org/gnome/settings-daemon/plugins/power" = {
+      power-button-action = "interactive"; # Ask for confirmation to shut down (power button is mapped on keyboard)
+      sleep-inactive-ac-type = "nothing"; # Never suspend desktop (Suspend is )
+    };
+    "org/gnome/desktop/session" = { "idle-delay" = 180; }; # NOT WORKING
+
+    # Mutlitasking
+    "org/gnome/desktop/interface" = {
+      "enable-hot-corners" = false; # Disable overview on hover in top-left corner
+      "color-scheme" = "prefer-dark"; # Use dark gnome theme
+    };
+    "org/gnome/mutter" = {
+      "edge-tiling" = true; # Drag windows to top, left and right to snap them
+      "dynamic-workspaces" = false; # Use fixed number of workspaces
+      "workspaces-only-on-primary" = true; # Workspaces affect only main monitor
+    };
+    "org/gnome/desktop/wm/preferences" = { "num-workspaces" = 6; }; # Amount of workspaces
+    "org/gnome/shell/app-switcher" = { "current-workspace-only" = false; }; # Allow cycling between apps on different workspaces
+
+    # Appearance
+    "org/gnome/desktop/interface" = { accent-color = "orange"; };
+    "org/gnome/desktop/background" = {
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+      picture-uri-dark = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-d.svg";
+      primary-color = "#241f31";
+    };
+    "org/gnome/desktop/screensaver" = {
+      picture-uri = "file:///run/current-system/sw/share/backgrounds/gnome/blobs-l.svg";
+      primary-color = "#241f31";
+    };
+
+    # Mouse and Touchpad
+    "org/gnome/desktop/peripherals/mouse" = {
+      left-handed = false;
+      speed = -0.22;
+      accel-profile = "default";
+      natural-scroll = false;
+    };
+    "org/gnome/desktop/peripherals/touchpad" = { send-events = "disabled"; };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      next = [ "<Shift>AudioPlay" ];
+      previous = [ "<Shift><Control>AudioPlay" ];
+      reboot = [ "<Control>PowerOff" ];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Control>grave" ];
+
+    };
+    "org/gnome/mutter" = { overlay-key = "Super"; };
+  };
+}

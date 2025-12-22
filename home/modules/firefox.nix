194f122c840632cf45d5361859a1ec11c523576f
wojtryb <wojtryb@gmail.com>
2025-12-22 15:30:34 +0100

    Update firefox configuration with extensions and search engines

--- home/modules/firefox.nix
+++ home/modules/firefox.nix
@@ -4,8 +4,38 @@
   pkgs,
   ...
 }:
-
+let
+  nixpkgsBaseVersion = builtins.head (builtins.match ''([0-9]+\.[0-9]+).*'' pkgs.lib.version);
+in
 {
+  options.firefox = {
+    userContent = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+    };
+  };
+
+  config.programs.firefox.policies = {
+    ExtensionSettings = {
+      "*".installation_mode = "allowed";
+      # Ad Blocker:
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      # Dark Reader:
+      "addon@darkreader.org" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      # YouTube extension for skipping irrelevant video parts
+      "sponsorBlocker@ajay.app" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
+  };
+
   config.programs.firefox.enable = true;
   config.programs.firefox.profiles."default-release" = {
     id = 0;
@@ -17,7 +47,7 @@
       "browser.aboutwelcome.enabled" = false;
       "extensions.autoDisableScopes" = 0;
       "ui.key.menuAccessKeyFocuses" = false;
-      "browser.startup.page" = 1;
+      "browser.startup.page" = 0;
       "browser.aboutConfig.showWarning" = false;
       "browser.tabs.closeWindowWithLastTab" = false;
       "browser.tabs.fadeOutUnloadedTabs" = true;
@@ -31,14 +61,11 @@
       "sidebar.expandOnHover" = false;
       "sidebar.verticalTabs" = true;
       "sidebar.position_start" = false; # Move to right
-      # "sidebar.animation.expand-on-hover.duration-ms" = 66;
-      # "sidebar.main.tools" = "syncedtabs,history,bookmarks";
       "sidebar.main.tools" = "";
       "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       "browser.uiCustomization.state" = {
         "placements" = {
           "nav-bar" = [
-            # "sidebar-button"
             "back-button"
             "forward-button"
             "stop-reload-button"
@@ -53,7 +80,6 @@
           ];
           "toolbar-menubar" = [ "menubar-items" ];
           "vertical-tabs" = [ "tabbrowser-tabs" ];
-          # "PersonalToolbar" = [ "personal-bookmarks" ];
         };
         "currentVersion" = 22;
         "newElementCount" = 5;
@@ -63,6 +89,7 @@
       "devtools.chrome.enabled" = true;
     };
 
+    # Is this not working?
     # non-clickable "mute/unmute" playback buttons on background tabs
     userChrome = ''
       tab.tabbrowser-tab:not([selected="true"]) image.tab-icon-overlay[role="presentation"] {
@@ -70,6 +97,68 @@
       }
     '';
 
+    userContent = lib.strings.concatStringsSep "\n" config.firefox.userContent;
     containersForce = true;
+
+    search.force = true;
+    search.engines."Nix Packages" = {
+      definedAliases = [ "@np" ];
+      urls = [
+        {
+          template = "https://search.nixos.org/packages";
+          params = [
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+            {
+              name = "channel";
+              value = nixpkgsBaseVersion;
+            }
+          ];
+        }
+      ];
+      iconMapObj."48" = "https://nixos.org/favicon-48x48.png";
+    };
+    search.engines."Nix Options" = {
+      definedAliases = [ "@no" ];
+      urls = [
+        {
+          template = "https://search.nixos.org/options";
+          params = [
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+            {
+              name = "channel";
+              value = nixpkgsBaseVersion;
+            }
+          ];
+        }
+      ];
+      iconMapObj."48" = "https://nixos.org/favicon-48x48.png";
+    };
+    search.engines."Home Manager Options" = {
+      definedAliases = [ "@hmo" ];
+      urls = [
+        {
+          template = "https://home-manager-options.extranix.com/";
+          params = [
+            {
+              name = "query";
+              value = "{searchTerms}";
+            }
+            {
+              name = "release";
+              value = "release-${nixpkgsBaseVersion}";
+            }
+          ];
+        }
+      ];
+      iconMapObj."48" = "https://nixos.org/favicon-48x48.png";
+    };
+
+    extensions.force = true;
   };
- }
+}
